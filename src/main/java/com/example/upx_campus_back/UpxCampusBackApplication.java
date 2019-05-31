625de50c73e29213a1cd80831aa5a6ad4ae96eaa
Durand Kévin <kevindrnd@live.fr>
2019-05-31 10:44:04 +0200

    Initial commit

---
+++ src/main/java/com/example/upx_campus_back/UpxCampusBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.upx_campus_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpxCampusBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpxCampusBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/upx_campus_back/model/Thematique.java
@@ -0,0 +1,4 @@
+package com.example.upx_campus_back.model;
+
+public class Thematique {
+}
