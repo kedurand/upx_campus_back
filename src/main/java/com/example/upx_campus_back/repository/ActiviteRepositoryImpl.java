b8b468e8c669979284b378ee13867d834c566ca4
Durand Kévin <kevindrnd@live.fr>
2019-05-31 13:40:30 +0200

    Init 2

--- src/main/java/com/example/upx_campus_back/repository/ActiviteRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.upx_campus_back.repository;
-
-import com.example.upx_campus_back.model.Batiment;
-
-import java.util.List;
-
-/* Sert d'interface avec la BDD (couche DAO) */
-public class ActiviteRepositoryImpl {
-}
--- src/main/java/com/example/upx_campus_back/service/ActiviteServiceImpl.java
+++ src/main/java/com/example/upx_campus_back/service/ActiviteServiceImpl.java
@@ -1,14 +1,12 @@
 package com.example.upx_campus_back.service;
 
 import com.example.upx_campus_back.model.Activite;
-import com.example.upx_campus_back.model.Batiment;
 import com.example.upx_campus_back.repository.ActiviteRepository;
-import com.example.upx_campus_back.repository.ActiviteRepositoryImpl;
 
 import java.util.List;
 
 public class ActiviteServiceImpl implements  ActiviteService{
-    private ActiviteRepositoryImpl activiteRepository;
+    private ActiviteRepository activiteRepository;
 
     public List<Activite> findAll() {
         return activiteRepository.findAll();
