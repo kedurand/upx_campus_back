7128e5b60eebc08dae5932671712b65fc3eae15a
aniesbel <anies-belh96@hotmail.fr>
2019-06-03 16:48:46 +0200

    Alimentation ActiviteController

--- src/main/java/com/example/upx_campus_back/controller/ActiviteController.java
+++ src/main/java/com/example/upx_campus_back/controller/ActiviteController.java
@@ -1,6 +1,8 @@
 package com.example.upx_campus_back.controller;
 
 import com.example.upx_campus_back.model.Activite;
+import com.example.upx_campus_back.model.Batiment;
+import com.example.upx_campus_back.model.Thematique;
 import com.example.upx_campus_back.service.ActiviteService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,17 @@ public class ActiviteController {
     @RequestMapping(value = "/activites", method = RequestMethod.GET)
     public List<Activite> getActivites(){
         return activiteService.getActivites();
+    }
+
+    @RequestMapping(value = "/addactivite/{id}/{text}/{thematique}/[{liseBatiment}]",method = RequestMethod.POST)
+    public ResponseEntity<Activite> saveActivite (@PathVariable Long id, @PathVariable String text, @PathVariable Thematique thematique, @PathVariable Batiment[] listeBatiment){
+        Activite activite = new Activite();
+        activite.setId(id);
+        activite.setText(text);
+        activite.setThematique(thematique);
+// a finir
+return null;
+
     }
 
   /*  @PostMapping
